2fbab50b350164707cb4f5ef5dd61534b631aa39
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-04-16 20:29:09 +0300

    base started

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      goit-react-hw-02-phonebook
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };  
+
+  render() {
+
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 28,
+          gap: '18px',
+          color: '#010101',
+        }}
+      >
+        <div>
+          goit-react-hw-02-phonebook
+        </div>
+      </div>
+    );
+  }
+}
+
